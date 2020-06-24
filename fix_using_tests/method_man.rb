a0adcc1d1628bb95df64fffdfbe56f763d60b9ab
Jennifer Yoo <jennifer.yoo7@gmail.com>
2020-06-24 01:54:35 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,8 +1,7 @@
 # don't forget to add: require 'pry'
 
 def start_game(player1, player2)
-  "Hello #{player1}"
-  "Hello #{playar2}"
+  "Hello #{player1} & #{player2}"
 end
 
 def play_game(player1, player2)
